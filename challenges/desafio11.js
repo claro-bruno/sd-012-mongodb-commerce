89d386960f13def6135c07d4a2c1d042572e85ce
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-10-13 12:55:46 -0300

    Desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,19 @@
+db.produtos.updateMany({},
+  {
+    $push:
+    {
+      tags:
+      {
+        $each: ["combo", "tasty"],
+        $sort: 1,
+      },
+    },
+  },
+  { collation: { locale: "pt" } });
+
+db.produtos.find({},
+  {
+    _id: false,
+    nome: true,
+    tags: true,
+  });
