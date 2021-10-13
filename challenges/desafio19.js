b9c2cc3086bb3d95ba3c441213627732e0b59c5c
Eder Santos <3derms@gmail.com>
2021-10-13 15:49:24 -0300

    Renomeia o campo descricao para descricaoSite em todos os documentos

---
+++ challenges/desafio19.js
@@ -0,0 +1,13 @@
+/** Source: https://docs.mongodb.com/manual/reference/operator/update/rename/ */
+db.produtos.updateMany(
+  {},
+  { $rename: {
+      descricao: "descricaoSite",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, descricao: 1, descricaoSite: 1 },
+);
