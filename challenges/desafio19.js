52c0943118c82232b87e03aafce6547dd717146c
Euller Braz <euller.braz99@gmail.com>
2021-10-13 16:28:47 -0300

    Desafios 18 a 22

---
+++ challenges/desafio19.js
@@ -0,0 +1,18 @@
+db.produtos.updateMany(
+  {},
+  {
+    $rename: {
+      descricao: "descricaoSite",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: true,
+    descricao: true,
+    descricaoSite: true,
+    _id: false,
+  },
+);
