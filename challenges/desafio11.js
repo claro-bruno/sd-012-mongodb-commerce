35e5445cf1ef77bfe4b22b31e79d9fbc2bfe8e25
Ramon Paschoal <rpaschoal90@gmail.com>
2021-10-13 14:39:13 -0300

    Requisito 11 ao 14

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -4,6 +4,7 @@ db.produtos.updateMany(
     $push: {
       tags: {
         $each: ["combo", "tasty"],
+        $sort: 1,
       },
     },
   },
--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,9 +1,11 @@
 db.produtos.updateMany(
   {
-    $and: [
-      { "valoresNutricionais.tipo": "sódio" },
-      { "valoresNutricionais.percentual": { $gte: 40 } }, 
-    ],
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: { $gte: 40 },
+      },
+    },
   },
   {
     $push: {
