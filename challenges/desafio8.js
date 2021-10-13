e695ba1166e288ef8a602119f1083c56739533cc
gabrielfndz <gabriel.omercador@hotmail.com>
2021-10-13 17:58:52 -0300

    requisito 8 e 9 refatoracao

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,4 +1,13 @@
-db.produtos.updateMany({ nome: "Quarteirão com Queijo" }, {
-  $pop: { ingredientes: -1 } });
+db.produtos.updateOne({
+  nome: "Quarteirão com Queijo",
+}, {
+  $pop: {
+    ingredientes: -1,
+  },
+});
 
-db.produtos.find({}, { _id: 1, nome: 1, ingredientes: 1 });
+db.produtos.find({}, {
+  _id: 1,
+  nome: 1,
+  ingredientes: 1,
+});
