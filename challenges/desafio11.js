e46e3ad00b97bd85ed67fa4501768780505b7d2a
camila-mp <ca.malvp@gmail.com>
2021-10-13 19:18:31 -0300

    corrige lint

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -9,10 +9,10 @@ db.produtos.updateMany(
   { $push:
     { tags:
       { $each: ["combo", "tasty"],
-        $sort: 1
-      }
-    }
-  }
+        $sort: 1,
+      },
+    },
+  },
 );
 
 db.produtos.find(
