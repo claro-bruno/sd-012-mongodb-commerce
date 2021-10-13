cae422299b5caa4463a831d8fc3d5dea61d5c43c
zevid-lucas <zevid.dev@outlook.com>
2021-10-13 14:48:25 -0300

    fix lint(7)

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,11 +1,11 @@
 db.produtos.updateMany({
-  nome: { $in: ["Big Mac", "Quarteirão com Queijo"] }
+  nome: { $in: ["Big Mac", "Quarteirão com Queijo"] },
 }, {
   $push: { ingredientes: "bacon" },
 });
 
 db.produtos.find({}, {
-  nome:1,
+  nome: 1,
   ingredientes: 1, 
   _id: 0,
 });
--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,11 +1,11 @@
-db.produtos.updateOne({nome: "Quarteirão com Queijo"}, {
+db.produtos.updateOne({ nome: "Quarteirão com Queijo" }, {
   $pop: {
-    ingredientes: -1
-  }
+    ingredientes: -1,
+  },
 });
 
 db.produtos.find({}, {
-  nome:1,
+  nome: 1,
   ingredientes: 1, 
   _id: 0,
 });
