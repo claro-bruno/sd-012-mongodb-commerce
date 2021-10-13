bed406c6398369164a30d80601647579c26e8f87
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-10-13 01:24:44 -0300

    First Commit

---
+++ challenges/desafio10.js
@@ -0,0 +1,30 @@
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      vendasPorDia: {
+        $each: [0, 0, 0, 0, 0, 0, 0],
+      },
+    },
+  },
+);
+
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  {
+    $inc: { "vendasPorDia.3": 60 },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino", "pão"] } },
+  {
+    $inc: { "vendasPorDia.6": 120 },
+  },
+);
+
+db.produtos.find({}, {
+  _id: false,
+  nome: true,
+  vendasPorDia: true,
+});
---
+++ challenges/desafio9.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany(
+  { nome: "Cheddar McMelt" },
+  {
+    $pop: {
+      ingredientes: 1,
+    },
+  },
+);
+
+db.produtos.find({}, {
+  _id: false,
+  nome: true,
+  ingredientes: true,
+});
