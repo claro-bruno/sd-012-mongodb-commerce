a64ecfbf2cb70404ddca049cc31a53f2e807af22
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-13 09:24:44 -0300

    desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  { nome: { $in: ["Quarteirão com Queijo", "Big Mac"] } },
+  {
+    $push: {
+      ingredientes: "bacon",
+    },
+  },
+);
+
+db.produtos.find(
+  { },
+  { _id: 0, nome: 1, ingredientes: 1 },
+);
