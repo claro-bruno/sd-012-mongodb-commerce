8918d1654a9cd97004b9a59300da3dbb2920f3be
Eder Santos <3derms@gmail.com>
2021-10-13 12:05:56 -0300

    corrige indentaçao em desafios

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,9 +1,9 @@
 db.produtos.updateMany(
-    { nome: { $nin: ["McChicken"] } },
-    { $addToSet: { ingredientes: "ketchup" } },
-  );
+  { nome: { $nin: ["McChicken"] } },
+  { $addToSet: { ingredientes: "ketchup" } },
+);
   
-  db.produtos.find(
-    {},
-    { _id: 0, nome: 1, ingredientes: 1 },
-  );
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, ingredientes: 1 },
+);
