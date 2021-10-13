64ec446922e74c10e9a2ddb7ea19b636450531ec
Luis Fernando Fonseca <78225894+luiszeh@users.noreply.github.com>
2021-10-13 18:23:41 -0300

    Update desafio21.js

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,5 +1,3 @@
+// gt = maior que - primeira posicao do array é a posicao que tem que ser maior, no caso $curtidas.
 db.produtos.find({ $expr: { $gt: ["$curtidas", "$vendidos"] } },
  { nome: 1, _id: 0 });
-
- // gt = maior que - primeira posicao do array é a posicao que tem que ser maior, no caso $curtidas.
- 
