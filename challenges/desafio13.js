736b99710b2d7dac32eb47cb0dac7a230d2c0f05
Rodrigo <rodrigo.facury14@gmail.com>
2021-10-13 18:30:03 -0300

    Refatora 13 e faz 14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,9 +1,7 @@
 db.produtos.updateMany({
-  $and: [
-    { "valoresNutricionais.tipo": "sódio" },
-    { "valoresNutricionais.percentual": { $gte: 40 } },
-    { tags: { $exists: true } },
-  ] },
+  valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
   { $push: { tags: "muito sódio" } });
 
 db.produtos.find({}, { nome: true, tags: true, _id: false }).pretty();
+
+// exercício realizado com a ajuda da colega Adriana Biberg.
