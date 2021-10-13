91ab805865957b8bbb98d5d11a5dea54552f87ed
Adriana Biberg <adribiberg@hotmail.com>
2021-10-13 16:02:57 -0300

    Desafio 9 - Remove o último ingrediente do sanduíche Cheddar McMelt

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
-    { nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: -1 } },
-    );
-    
-    db.produtos.find({}, { nome: true, ingredientes: true, _id: false });
+  { nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: -1 } },
+  );
+  
+  db.produtos.find({}, { nome: true, ingredientes: true, _id: false });
