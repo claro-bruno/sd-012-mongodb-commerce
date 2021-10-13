33f8aabe7575ebd7741d46cca01c113fb7a7efd1
Thiago Prado <thiagopc_dev@hotmail.com>
2021-10-13 14:49:23 -0300

    fix-eslint def-1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,9 +1,9 @@
 const filters = {};
 const fields = {
   $set: {
-    criadoPor: "Ronald McDonald"
-  }
+    criadoPor: "Ronald McDonald",
+  },
 };
 
 db.produtos.updateMany(filters, fields);
-db.produtos.find({}, {nome: 1, criadoPor: 1, _id: 0});
+db.produtos.find({}, { nome: 1, criadoPor: 1, _id: 0 });
