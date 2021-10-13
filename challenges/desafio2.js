0c95f6ff2a1ea48478e06ddc44acf59596e7b804
zevid-lucas <zevid.dev@outlook.com>
2021-10-13 14:29:12 -0300

    Done challenge2, and fix eslint(2)

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,8 +1,8 @@
 db.produtos.updateMany({
-  valorUnitario: {$exists: false},
+  valorUnitario: { $exists: false },
 }, {
   $set: {
-    valorUnitario: NumberDecimal("0.00")
-},});
+    valorUnitario: NumberDecimal("0.00"),
+}, });
 
-db.produtos.find({}, {nome: 1, valorUnitario:1, _id: 0});
+db.produtos.find({}, { nome: 1, valorUnitario:1, _id: 0 } );
