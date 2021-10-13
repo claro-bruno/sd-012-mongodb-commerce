c28aa3317a0155258c885b714765d2d5f885116e
gabrielfndz <gabriel.omercador@hotmail.com>
2021-10-13 18:28:19 -0300

    Requisitos

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,18 +1,3 @@
-db.produtos.updateOne(
-{
-  nome: "Quarteirão com Queijo",
-}, {
-  $pop: {
-    ingrediente: -1,
-  },
-},
-);
+db.produtos.updateOne({ nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: -1 } });
 
-db.produtos.find(
-  {}, 
-  {
-  _id: 1,
-  nome: 1,
-  ingredientes: 1,
-},
-);
+db.produtos.find({}, { _id: 1, nome: 1, ingredientes: 1 });
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,6 +1,3 @@
-db.produtos.updateOne({ 
-  nome: "Cheddar McMelt" }, {
-  $pop: { ingrediente: 1 },
-});
+db.produtos.updateOne({ nome: "Cheddar McMelt" }, { $pop: { ingredientes: 1 } });
 
 db.produtos.find({}, { _id: 1, nome: 1, ingredientes: 1 });
