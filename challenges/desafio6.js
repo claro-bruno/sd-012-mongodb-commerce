1e726d678ec6ad006ef4c7e9e67d2d92ba2f9589
BSY-Development <bes.yamamoto@gmail.com>
2021-10-13 15:11:11 -0300

    Requisito 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,1 +1,26 @@
-// Checando evaluator
+db.produtos.updateMany(
+  {
+    $or: [
+      {
+        nome: "Big Mac",
+      },
+      {
+        nome: "Quarteirão com Queijo",
+      },
+    ],
+  },
+  {
+    $addToSet: {
+      ingredientes: "bacon",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: 1,
+    ingredientes: 1,
+    _id: 0,
+  },
+);
