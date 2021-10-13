caadd4284127176312e24986db3a8eab369084c2
Eric Faria <ericdeofaria@gmail.com>
2021-10-13 15:02:31 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,24 @@
+db.produtos.update(
+  {
+    nome: "Big Mac",
+  },
+  {
+    $currentDate:
+      {
+        ultimaModificacao: true,
+      },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao:
+      {
+        $exists: true,
+      },
+  },
+  {
+    _id: false,
+    nome: true,
+  },
+);
