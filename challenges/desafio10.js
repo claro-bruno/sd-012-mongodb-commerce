67a3e67db8f6774b9f1dd083302b5f42ca982d60
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-10-13 17:55:01 -0300

    completa req 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,41 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
+    },
+  },
+  {
+    upsert: true,
+  },
+);
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $set: {
+      "vendasPorDia.3": 60,
+    },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: {
+      $all: ["bovino", "pão"],
+    },
+  },
+  {
+    $set: {
+      "vendasPorDia.6": 120,
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: 1,
+    vendasPorDia: 1,
+    _id: 0,
+  },
+);
