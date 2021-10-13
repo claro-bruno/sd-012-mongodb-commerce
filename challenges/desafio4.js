a7651ca79f78e7fc7f2c4882f618d1a5b38f2788
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-10-13 15:50:30 -0300

    completa req 04

---
+++ challenges/desafio4.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  {
+    $currentDate: { ultimaModificacao: { $type: "date" } },
+  },
+  { upsert: true },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  {
+    nome: 1,
+    _id: 0,
+  },
+);
