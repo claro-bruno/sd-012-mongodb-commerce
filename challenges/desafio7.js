cd3f2cef113c47774bcffa723d52514b1a924e02
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-10-13 15:08:33 -0300

    Realiza desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany(
+  {
+
+  },
+  {
+    $pull: {
+      ingredientes: "cebola",
+    },
+  },
+);
+
+db.produtos.find(
+  {
+
+  },
+  {
+    nome: true,
+    ingredientes: true,
+    _id: false,
+  },
+);
