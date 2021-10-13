f0f392c49e9494f5bb8f404d00de1dfff9c2a9cb
cristianocsl <cslcristiano@gmail.com>
2021-10-13 16:24:32 -0300

    desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+  { ingredientes: { $in: ["cebola"] } },
+  {
+    $pull: { ingredientes: "cebola" },
+  },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, ingredientes: 1, _id: 0 },
+);
