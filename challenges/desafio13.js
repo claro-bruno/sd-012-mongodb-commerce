f360d60425e44d97ef17adb10ac07091c011a829
José Reginaldo Da Silva Neto <netosilv44445@gmail.com>
2021-10-13 15:21:26 -0300

    dale

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany({ valoresNutricionais: 
-  { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } }}
-}, { $push: { tags: "muito sódio" }})
+  { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } },
+}, { $push: { tags: "muito sódio" } });
 
-db.produtos.find({}, { nome: 1, _id: 0, tags: 1})
+db.produtos.find({}, { nome: 1, _id: 0, tags: 1 });
