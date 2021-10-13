3993c7632b7bed7ca7ec188a26dca59baef04c13
Gabriel Takaki <gabitj@icloud.com>
2021-10-13 17:01:55 -0300

    desafios 10-15

---
+++ challenges/desafio14.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: { $gt: 20, $lt: 40 },
+      },
+    },
+  },
+  { $push: { tags: "contém sódio" } }
+);
+
+db.produtos.find({}, { tags: 1, _id: 0, nome: 1 });
