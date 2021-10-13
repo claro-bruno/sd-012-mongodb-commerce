976869eb201a9711395fcee7bdbee11603b641bf
Renan Ferreira <renansantiago2013@hotmail.com>
2021-10-13 16:09:15 -0300

    Resolve o requisito 10

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,11 +1,11 @@
 db.produtos.updateMany(
-    { nome: "Cheddar McMelt" },
-    { $pop: {
-        ingredientes: 1, 
-    } },
-    );
-    
-    db.produtos.find(
-    { },
-    { _id: 0, nome: 1, ingredientes: 1 },
-    );
+{ nome: "Cheddar McMelt" },
+{ $pop: {
+    ingredientes: 1, 
+} },
+);
+
+db.produtos.find(
+{ },
+{ _id: 0, nome: 1, ingredientes: 1 },
+);
