3c76f6bb0a009a85f10e02f6bba2219c8a426227
Rodrigo <rodrigo.facury14@gmail.com>
2021-10-13 15:38:49 -0300

    Desafio 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,1 +1,7 @@
-db.produtos.find();
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
+
+db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
+
+db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliacao: 3 } });
+
+db.produtos.find({}, { nome: true, avaliacao: true, _id: false }).pretty();
