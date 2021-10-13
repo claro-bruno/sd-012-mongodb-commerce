bcc70e303eb3cb2876bd3fa3929e5d83c046b1da
tomsantos07 <xotmsantosx@live.com>
2021-10-13 19:06:23 -0300

    requisito 03

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,3 +1,11 @@
-// db.produtos.updateMany(
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: NumberInt(0) } },
+);
 
-// )
+db.produtos.updateMany(
+  { tags: { $all: ["bovino" ] } },
+  { $inc: { avalicao: 5 } },
+);
+
+db.produtos.find({}, { _id: 0, nome:1, avaliacao: 1 } );
