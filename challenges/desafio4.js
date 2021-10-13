7a43f0dbf6bec7618659198ad5f7ee5f94645efc
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-10-13 17:53:48 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany(
+    { nome: "Big Mac" },
+    { $currentDate: { 
+        ultimaModificacao: { $type: "date" },
+      },
+    },
+  );
+  
+  db.produtos.find(
+    { ultimaModificacao: { $exists: true } },
+    {
+      _id: 0,
+      nome: 1,
+    },
+  );
