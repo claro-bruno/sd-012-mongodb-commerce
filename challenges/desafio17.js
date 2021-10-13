5162a17f38dc1a42731b574107b6e0da876f79aa
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-10-13 19:13:11 -0300

    Fix Requisitos 17, 18 e 19

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,9 +1,9 @@
-db.produtos.createIndex({ 
-  descricao: "text",
- default_language: "portuguese",
-});
-db.produtos.count({ 
+db.produtos.createIndex( 
+ { descricao: "text" },
+ { default_language: "portuguese" },
+);
+db.produtos({ 
   $text: { 
     $search: "frango hamburguer",
   },
-});
+}).coun();
