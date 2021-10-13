c4b5f871961572e2485b0ba91346c2a50e0b0b84
camila-mp <ca.malvp@gmail.com>
2021-10-13 16:58:22 -0300

    resolve desafio4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -3,3 +3,13 @@
 
 //1-Crie uma query que atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac. Para a data corrente faça uso do tipo Date.
 //2-Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
+
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: true } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 },
+);
