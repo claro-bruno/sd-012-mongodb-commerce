37d53f61650573535b6ea5bcc1be269715d59b9e
Wadson Almeida <wadson.dev92@gmail.com>
2021-10-13 17:48:34 -0300

    Resolve desafio4

---
+++ challenges/desafio4.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  {
+    $set: {
+      ultimaModificacao: Date() },
+    },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: 1 } },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
