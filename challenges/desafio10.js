9e657b2e619aa7795e4a433c768399e61f0ada00
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-10-13 12:19:07 -0300

    Desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany({},
+  {
+     $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] },
+  });
+
+db.produtos.update({ nome: "Big Mac" },
+  {
+    $set: { "vendasPorDia.3": 60 },
+  });
+
+db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } },
+  {
+    $set: { "vendasPorDia.6": 120 },
+  });
+
+db.produtos.find({},
+  {
+    _id: false,
+    nome: true,
+    vendasPorDia: true,
+  });
