67ce4862542fde2bb491d39302ec27a1d70bb2a6
Isaac Cruz <isaac.clm1@gmail.com>
2021-10-13 17:43:00 -0300

    EDIT: Requisito 19

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -2,7 +2,7 @@ db.produtos.updateMany(
   { },
   { 
     $rename: {
-      descricao: descricaoSite,
+      descricao: "descricaoSite",
     },
   },
 );
@@ -12,6 +12,7 @@ db.produtos.find(
   {
     _id: 0,
     nome: 1,
+    descricao: 1,
     descricaoSite: 1,
   },
 );
