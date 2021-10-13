00e01051c1d1de2a51c98b7292b08bee2f83799b
zevid-lucas <zevid.dev@outlook.com>
2021-10-13 14:26:55 -0300

    Fix eslint(1)

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -3,7 +3,7 @@ db.produtos.updateMany({},
     $set: {
       criadoPor: "Ronald McDonald",
     },
-  }
+  },
 );
 
 db.produtos.find(
@@ -12,6 +12,5 @@ db.produtos.find(
     nome: 1,
     criadoPor: 1,
     _id: 0,
-  }
+  },
 );
-
