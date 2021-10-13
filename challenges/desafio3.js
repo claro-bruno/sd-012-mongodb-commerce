6ca52f0f4168a0f0e422c99c363b6b9da5b8dbe2
André Moreno <deco616@hotmail.com>
2021-10-13 15:47:41 -0300

    Desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany(
+  { },
+  { 
+    $set: { avaliacao: 0 },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["bovino"] } },
+  { $inc: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["ave"] } },
+  { $inc: { avaliacao: 3 } },
+);
+
+db.produtos.find(
+  { },
+  { nome: 1, _id: 0, avaliacao: 1 },
+);
