c212f8e305faeb7b2bda1af8bada4ea46302f47f
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-10-13 16:22:55 -0300

    adiciona desafio 13

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,12 +2,12 @@ db.produtos.updateMany({},
   {
     $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] },
   });
-  db.produtos.updateMany({ nome: "Big Mac" },
+db.produtos.updateMany({ nome: "Big Mac" },
     {
       $inc: { "vendasPorDia.3": 60 },
     });
-  db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } },
+db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } },
     {
       $inc: { "vendasPorDia.6": 120 },
     });
-  db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
+db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
