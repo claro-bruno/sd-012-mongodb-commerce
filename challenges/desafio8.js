106f200a39257c2b54802567a9904f99e095cea1
gabrielfndz <gabriel.omercador@hotmail.com>
2021-10-13 17:51:40 -0300

    Requisito 8 e 9 refatorado

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,6 +1,4 @@
-db.produtos.updateOne({ nome: "Quarteirão com Queijo" }, {
-  $pop: {
-    ingredientes: -1 },
-});
+db.produtos.updateMany({ nome: "Quarteirão com Queijo" }, {
+  $pop: { ingredientes: -1 } });
 
 db.produtos.find({}, { _id: 1, nome: 1, ingredientes: 1 });
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,6 +1,7 @@
-db.produtos.updateOne({ nome: "Cheddar McMelt" }, {
+db.produtos.updateMany({ nome: "Cheddar McMelt" }, {
   $pop: {
-    ingredientes: 1 },
+    ingredientes: 1,
+  },
 });
 
 db.produtos.find({}, { _id: 1, nome: 1, ingredientes: 1 });
