d9555949caed7fff51b75fb39dd9e0e521594874
julio11lp <julio11lp@hotmail.com>
2021-10-13 19:25:28 -0300

    Desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  {
+    nome: { $ne: "McChicken" },
+  },
+  {
+    $addToSet: { ingredientes: "ketchup" },
+  },
+  {
+    $upsert: true,
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: true,
+    ingredientes: true,
+    _id: false,
+  },
+);
