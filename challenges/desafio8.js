3acf4aa99e2f06cb84ca8d44022cf407ba658bf9
Euller Braz <euller.braz99@gmail.com>
2021-10-13 15:44:09 -0300

    Desafios 7 a 12

---
+++ challenges/desafio8.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany(
+  {
+    nome: {
+      $eq: "Quarteirão com Queijo", 
+    },
+  },
+  {
+    $pop: {
+      ingredientes: -1,
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: true,
+    ingredientes: true,
+    _id: false,
+  },
+);
