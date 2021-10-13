65169e89821eefe08affe1ad585f50455e54fe44
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-10-13 10:31:50 -0300

    conclui requisito 2 - Inclua o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.

---
+++ challenges/desafio2.js
@@ -0,0 +1,24 @@
+db.produtos.updateMany(
+  {
+      valorUnitario: {
+        $exists: false,
+      },
+  },
+  {
+    $set: {
+      valorUnitario: NumberDecimal("0.00"),
+    },
+  },
+  {
+    upsert: true,
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: 1,
+    valorUnitario: 1,
+    _id: 0,
+  },
+);
