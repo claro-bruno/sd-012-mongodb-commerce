2c3e033ad16e924d6fd1d551e4ddd8b4c98938be
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-10-13 18:15:41 -0300

    Desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,22 @@
+// 11 - Insira os elementos `combo` e `tasty` no _array_ `tags` 
+// de todos os sanduíches e aproveite para deixar os elementos em ordem alfabética ascendente.
+
+db.produtos.updateMany({},
+  {
+    $push:
+    {
+      tags:
+      {
+        $each: ["combo", "tasty"],
+        $sort: 1,
+      },
+    },
+  },
+  { collation: { locale: "pt" } });
+
+db.produtos.find({},
+  {
+    _id: false,
+    nome: true,
+    tags: true,
+  });
