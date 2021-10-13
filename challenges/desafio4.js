82e44662724d9d4c0f0a57ddbc78e7f806c527f5
camila-mp <ca.malvp@gmail.com>
2021-10-13 16:59:20 -0300

    corrige lint

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,8 +1,8 @@
 // Atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac.
 // Para isso, escreva no arquivo desafio4.js duas queries, nesta ordem:
 
-//1-Crie uma query que atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac. Para a data corrente faça uso do tipo Date.
-//2-Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
+// 1-Crie uma query que atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac. Para a data corrente faça uso do tipo Date.
+// 2-Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
 
 db.produtos.updateMany(
   { nome: "Big Mac" },
