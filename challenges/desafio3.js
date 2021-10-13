30b82cfa175acb809f69eca6fef0ae2dff5de90b
zevid-lucas <zevid.dev@outlook.com>
2021-10-13 14:38:42 -0300

    fix linter(5)

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,25 +1,26 @@
 db.produtos.updateMany({},
   {
-    $set: {avaliacao: NumberInt(0)}
+    $set: { avaliacao: NumberInt(0) },
   });
 
   db.produtos.updateMany({
-    tags: {$elemMatch: { $eq: "bovino" } }
+    tags: { $elemMatch: { $eq: "bovino" } },
   }, {
     $inc: {
-      avaliacao: 5
-    }
+      avaliacao: 5,
+    },
   });
 
   db.produtos.updateMany({
-    tags: {$elemMatch: { $eq:"ave" } }
+    tags: { $elemMatch: { $eq:"ave" } },
   }, {
     $inc: {
-      avaliacao: 3
+      avaliacao: 3,
     }
   });
   
-
-  db.produtos.find({}, {
-    nome: 1, avaliacao: 1, _id: 0
+  db.produtos.find({}, 
+  {
+    nome: 1, avaliacao: 1, _id: 0,
   });
+
