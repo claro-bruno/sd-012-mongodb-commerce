d6ba1bc28fadd034bc696ba60717da00457b36e0
Renan Ferreira <renansantiago2013@hotmail.com>
2021-10-13 15:39:29 -0300

    Resolve o requisito 9

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,11 +1,11 @@
 db.produtos.updateMany(
-    { nome: "Quarteirão com Queijo" },
-    { $pop: {
-        ingredientes: -1, 
-    } },
-    );
-    
-    db.produtos.find(
-    { },
-    { _id: 0, nome: 1, ingredientes: 1 },
-    );
+{ nome: "Quarteirão com Queijo" },
+{ $pop: {
+    ingredientes: -1, 
+} },
+);
+
+db.produtos.find(
+{ },
+{ _id: 0, nome: 1, ingredientes: 1 },
+);
