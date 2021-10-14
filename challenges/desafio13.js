e778167eb87c38cd141dfbc8bfc867346b7d1d49
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-10-14 08:03:06 -0300

    Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,27 @@
+// 13 - Adicione o elemento `muito sódio` ao final do _array_ `tags` 
+// nos produtos em que o `percentual` de `sódio` seja maior ou igual a `40`.
+
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch:
+      {
+        tipo: "sódio",
+        percentual: { $gte: 40 },
+      },
+    },
+  },
+  {
+    $push:
+    {
+      tags: "muito sódio",
+    },
+  },
+);
+
+db.produtos.find({},
+  {
+    _id: false,
+    nome: true,
+    tags: true,
+  });
