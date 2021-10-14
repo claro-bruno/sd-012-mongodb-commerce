5873e8f9eaed52528dab6ea73d566ce38181d088
Daniel Ceci <danielceci96@gmail.com>
2021-10-13 21:42:10 -0300

    requisito 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+    {}, 
+    { $pull: { ingredientes: ["cebola"] } },
+); // o pull remove elementos de array. Nesse caso, quero tirar o elemento "cebola" dentro do ingredientes. O filtro é vazio, porque quero retirar cebola do array ingredientes em todos os objetos dentro da coleção PRODUTOS.
+
+db.produtos.find(
+    {},
+    { 
+        nome: true,
+        ingredientes: true,
+        _id: false,
+    },
+);
