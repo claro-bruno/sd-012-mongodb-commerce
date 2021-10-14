136ec059a7de63dfddd121e3d5a39175d5e8a7fe
Antonio Malato <malatoneto@gmail.com>
2021-10-14 09:24:15 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,24 @@
+db.produtos.updateOne(
+  {
+    nome: "Big Mac",
+  },
+  {
+    $set: {
+      ultimaModificacao: {
+       currentDate: Date(),
+     },
+   },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: {
+      $exists: 1,
+    },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
