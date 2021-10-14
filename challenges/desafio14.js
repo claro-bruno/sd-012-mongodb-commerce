af7e9a9570cb3318e6b03507c575192e0aecf648
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-10-14 08:07:10 -0300

    Desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,27 @@
+// 14 - Adicione o elemento `contém sódio` ao final do _array_ `tags` nos produtos em que o 
+// `percentual` de `sódio` seja maior do que `20` e menor do que `40`.
+
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch:
+      {
+        tipo: "sódio",
+        percentual: { $gt: 20, $lt: 40 },
+      },
+    },
+  },
+  {
+    $push:
+    {
+      tags: "contém sódio",
+    },
+  },
+);
+
+db.produtos.find({},
+  {
+    _id: false,
+    nome: true,
+    tags: true,
+  });
