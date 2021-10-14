0e52cc5c82992e6a8f45c112e62b58192b049d6b
Wadson Almeida <wadson.dev92@gmail.com>
2021-10-13 22:59:30 -0300

    Resolve desafio20

---
+++ challenges/desafio20.js
@@ -0,0 +1,17 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $unset: {
+      curtidas: true,
+    },
+  },
+);
+
+db.produtos.find(
+  { },
+  {
+    _id: 0,
+    nome: 1,
+    curtidas: 1,
+  },
+);
