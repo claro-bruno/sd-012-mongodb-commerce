842d40f92201b5e08ae051c2ea9b56c31efefc24
Tiago Ornelas <advtiagoornelas@gmail.com>
2021-10-14 16:09:36 -0300

    Resolve o requisito 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany(
+  {},
+  { $set: { criadoPor: "Ronald McDonald" } },
+  { upsert: true },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    criadoPor: true,
+  },
+);
