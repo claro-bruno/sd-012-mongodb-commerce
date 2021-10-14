162a9e2178db8243b21eacebd918aa8f3681db59
Tiago Ornelas <advtiagoornelas@gmail.com>
2021-10-14 16:57:42 -0300

    Resolve o requisito 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $currentDate: {
+      ultimaModificacao: true,
+    },
+  },
+);
+
+db.produtos.find(
+  { ultimaModificacao: {
+    $exists: true,
+  } },
+  {
+    _id: false,
+    nome: true,
+  },
+);
