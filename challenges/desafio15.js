4d43a0bea09b4bedc8b82e621d8b569a57c4dc8a
Daniel Ceci <danielceci96@gmail.com>
2021-10-14 14:04:26 -0300

    requisito 15

---
+++ challenges/desafio15.js
@@ -0,0 +1,4 @@
+db.produtos.count({ nome: { $regex: /Mc/i } });
+// usei o count porque o req 15 pede contagem
+// a letra i é pra indicar case sensitive, aí não vai importar ser letra maiúscula ou minúscula
+// Não usei ^ ou $ no regex porque o req não especificou se queria que começasse ou terminasse com Mc.
