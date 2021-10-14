1321f60856e6515cf62b23aa6d6211312148aea4
johnpligeiro <johnpligeiro@gmail.com>
2021-10-14 15:25:23 -0300

    Refatora com addToSet

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,14 +1,11 @@
 db.produtos.updateMany(
-  { 
-    $and: [
-      { nome: { $not: { $eq: "McChicken" } } },
-      { ingredientes: { $not: { $in: ["ketchup"] } } },
-   ],
-  },
-  { $push: { ingredientes: "ketchup" } },
-  );
+  { nome: { $ne: "McChicken" } },
+  { $addToSet: { ingredientes: "ketchup" } },
+);
 
-  db.produtos.find(
-    { },
-    { nome: true, ingredientes: true, _id: false },
-  );
+db.produtos.find(
+  { },
+  { nome: true, ingredientes: true, _id: false },
+);
+
+// addToSet - refatoração - não adiciona duplicado, eliminando a necessidade de filtrar
