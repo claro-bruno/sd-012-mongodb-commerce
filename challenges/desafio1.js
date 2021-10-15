de04b53d57ea0f59df29b0e795f43fc374705ac8
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-10-15 18:09:49 -0300

    desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,15 @@
-db.voos.find().count();
+db.produtos.updateMany({}, {
+  /**
+   * field: The field name
+   * expression: The expression.
+   */
+  $set: {
+    criadoPor: "Ronald McDonald",
+  },
+});
+
+  db.produtos.find({}, {
+    _id: 0,
+    nome: 1,
+    criadoPor: 1,
+  });
