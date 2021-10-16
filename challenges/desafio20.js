0dabced1214a174329da8acb335b04282fc784c4
ArturAlvaro <artur.alvaro73@gmail.com>
2021-10-16 16:56:52 -0300

    Requisito 20.1

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,10 +1,10 @@
-db.produtos.updateMany(
-  {},
+db.produtos.updateOne(
+  { nome: "Big Mac" },
   {
     $unset: {
       curtidas: "",
     },
   },
 );
-
+use("commerce");
 db.produtos.find({}, { _id: 0, nome: 1, curtidas: 1 });
