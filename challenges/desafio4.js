a4fb585ac2b1bff2778e61ba411515bd09cd41f0
martinsph <ph.mmartins@gmail.com>
2021-10-16 19:43:44 -0300

    add req 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  // { $currentDate: { ultimaModificacao: 1 } }
+  { $currentDate: { ultimaModificacao: { $type: "date" } } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: 1 } },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
