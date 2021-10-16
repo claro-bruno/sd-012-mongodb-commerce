abc75b9cfcc2cbb5af76e2ee8916f9655d572b0f
martinsph <ph.mmartins@gmail.com>
2021-10-16 20:18:23 -0300

    add req 9

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
-  { nome: "Cheddar McMelt" },
-  { $pop: { ingredientes: 1 } },
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } },
 );
 
 db.produtos.find(
