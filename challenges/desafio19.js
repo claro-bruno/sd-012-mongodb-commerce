cf54af9758e82562753bbd0445446dc4caa6e992
thales <thalesdaniel2013@gmail.com>
2021-10-16 17:40:15 -0300

    feito desafio 18 e 19

---
+++ challenges/desafio19.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {},
+  {
+    $rename: { descricao: "descricaoSite" },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: 1,  
+    _id: 0,
+    descricao: 1,
+    descricaoSite: 1, 
+  },
+);
