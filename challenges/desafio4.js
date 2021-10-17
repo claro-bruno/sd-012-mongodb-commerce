0f67ce1202d3c4b57acf7f9458e83b3187ea6754
Marcos Mantovani <mvmes23@gmail.com>
2021-10-17 07:41:53 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  {
+    $set: {
+      ultimaModificacao: new Date(),
+    },
+  },
+);
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: false, nome: true },
+);
