f952f3df939c84cea3c0acbbc6651996d7ccbfc2
Marcos Mantovani <mvmes23@gmail.com>
2021-10-17 08:35:37 -0300

    Desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  { },
+  {
+    $push: {
+      tags: {
+        $each: ["combo", "tasty"],
+        $sort: 1,
+      },
+    },
+  },
+  { collation: { locale: "pt" } },
+);
+db.produtos.find(
+  { },
+  { _id: false, nome: true, tags: true },
+);
