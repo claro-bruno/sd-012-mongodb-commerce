aa2336d20838c8dd3ab1f3fca88ddffac269cff5
Uriel <urielbochi@gmail.com>
2021-10-16 22:44:39 -0300

    correção 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,6 +1,8 @@
-db.produtos.updateMany({ nome: "Big Mac" },
- { $currentDate: { ultimaModificacao: { type: "date" } } },
- { $upsert: true });
+db.produtos.updateMany(
+ { nome: "Big Mac" },
+ { $currentDate: { ultimaModificacao: { $type: "date" } } },
+ { $upsert: true },
+ );
 
  db.produtos.find(
    { ultimaModificacao: { $exists: true } },
