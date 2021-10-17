8b253630cfda87993224f811e36194c49e26c2bb
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-10-17 11:28:59 -0300

    desafio 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,12 @@
-db.voos.find({
-  "empresa.nome": "GOL",
-}).count();
+db.produtos.updateOne({ nome: "Big Mac" },
+ {  
+    $currentDate: { ultimaModificacao: { $type: "date" } },
+  });
+
+db.produtos.find({
+  ultimaModificacao: { $exists: true },
+},
+ {
+    _id: 0,
+    nome: 1,
+  });
