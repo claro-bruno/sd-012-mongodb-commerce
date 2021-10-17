f8935b252ceddc6202c7d790df934fdd74e13526
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-10-17 13:05:45 -0300

    desafio 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,3 +1,15 @@
-db.voos.find(
-  { "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } },
-).count();
+db.produtos.updateMany({ valoresNutricionais: { $elemMatch: { 
+  tipo: "sódio",
+  percentual: { $gte: 40,
+  } } } },
+  {  
+  $push: { tags: { $each: ["muito sódio"] } },
+  
+});
+db.produtos.find({
+},
+ {
+  _id: 0,
+  nome: 1,
+  tags: 1,
+});
