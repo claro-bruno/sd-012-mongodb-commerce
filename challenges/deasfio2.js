89eb57a78f7ad8ff9cd95518210d1f76a7a1e32a
Uriel <urielbochi@gmail.com>
2021-10-16 21:05:36 -0300

    desafio 2 feito

---
+++ challenges/deasfio2.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+  {}, { $set: {
+    valorUnitario: NumberDecimal("0.00"),
+  } },
+  { $upsert: true },
+);
+
+db.produtos.find({}, {
+nome: true,
+valorUnitario: true,
+_id: false,
+});
