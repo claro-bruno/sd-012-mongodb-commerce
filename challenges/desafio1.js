1e8f621efce7978cd6dd4f32a61081bf90dcd618
Uriel <urielbochi@gmail.com>
2021-10-16 21:06:39 -0300

    meu deus pq eu coloquei aquilo ali, req 1 + 2 (talvez)

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } }, { $upsert: true });
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
 db.produtos.find({}, { nome: true, criadoPor: true });
