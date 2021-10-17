f6f44043c00cb91b60a9490652df3b7c1b06dad6
Uriel <urielbochi@gmail.com>
2021-10-16 21:36:32 -0300

    tentativa correção 2

--- challenges/deasfio2.js
+++ challenges/deasfio2.js
@@ -1,11 +1,13 @@
 db.produtos.updateMany(
-  {}, { $set: {
-    valorUnitario: NumberDecimal("0.00"),
-  } },
+  {},
+   { $set: {
+    valorUnitario: NumberDecimal("0.00") },
+  },
   { $upsert: true },
 );
 
-db.produtos.find({}, {
+db.produtos.find({},
+   {
 nome: true,
 valorUnitario: true,
 _id: false,
