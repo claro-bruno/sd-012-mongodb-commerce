4f8ed6395ccb7441cad668a6763fa0a204f86727
camila-mp <ca.malvp@gmail.com>
2021-10-17 19:02:03 -0300

    resolve desafio17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -3,3 +3,16 @@
 
 // Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
 // Crie uma query que retorne a quantidade de documentos que contêm as palavras frango ou hamburguer utilizando o operador $text.
+
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "pt" }
+);
+
+db.produtos.count(
+  {
+    $text: {
+      $search: "frango hamburger"
+    },
+  },
+);
