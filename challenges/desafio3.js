2b90a6ee6d06ca762ee5abc77fc615d2960bea47
Marcos Mantovani <mvmes23@gmail.com>
2021-10-17 07:33:53 -0300

    Desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,28 @@
+db.produtos.updateMany(
+  { },
+  {
+    $set: {
+      avaliacao: NumberInt("0"),
+    },
+  },
+);
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] } },
+  {
+    $set: {
+      avaliacao: NumberInt("5"),
+    },
+  },
+);
+db.produtos.updateMany(
+  { tags: { $in: ["ave"] } },
+  {
+    $set: {
+      avaliacao: NumberInt("3"),
+    },
+  },
+);
+db.produtos.find(
+  { },
+  { _id: false, nome: true, avaliacao: true },
+);
