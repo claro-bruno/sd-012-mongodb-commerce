e2bf6094c9babce2c1aaf8ce5572af048be80065
Uriel <urielbochi@gmail.com>
2021-10-16 22:29:24 -0300

    desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany({},
+  { $set: { avaliacao: NumberInt(0) } },
+  { $upsert: true });
+
+db.produtos.updateMany({ tags: { $in: ["bovino"] } },
+  { $inc: { avaliacao: 5 } });
+
+db.produtos.updateMany({ tags: { $in: ["ave"] } },
+{ $inc: { avaliacao: 3 } });
+
+db.produtos.find({
+  nome: true,
+  avaliacao: true,
+  _id: false,
+});
