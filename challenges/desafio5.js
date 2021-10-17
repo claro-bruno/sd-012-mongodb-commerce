bee31928c7e64ce2f45882c4491cda52c7a2d016
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-10-17 16:16:12 -0300

    Correçao Requisitos 4 e 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -3,7 +3,7 @@ db.produtos.updateMany({
     $and: [
       { nome: { $not: { $eq: "McChicken" } } },
       { ingredientes: { $not: { $in: ["ketchup"] } } },
-    ]
+    ],
 }, { $push: { ingredientes: "ketchup" } });
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
