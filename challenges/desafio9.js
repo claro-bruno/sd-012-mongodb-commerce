1dd10a9fa2fba950a4cf1205846ecc132821bd36
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-10-17 12:02:44 -0300

    desafio 9

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,3 +1,12 @@
-db.voos.find(
-  { ano: { $gte: 2017, $lte: 2018 } },
-).count();
+db.produtos.updateMany({ nome: "Cheddar McMelt" },
+ {  
+    $pop: { ingredientes: 1 },
+  });
+
+db.produtos.find({
+},
+ {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+  });
