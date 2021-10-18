48d5bd847336880c1ca5f06f9e73b96e27115987
camila-mp <ca.malvp@gmail.com>
2021-10-18 10:15:41 -0300

    modifica o desafio17 pra ver se o avaliador doido reconhece que ta certo...

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -4,13 +4,14 @@
 // Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
 // Crie uma query que retorne a quantidade de documentos que contêm as palavras frango ou hamburguer utilizando o operador $text.
 
-db.produtos.createIndex(
+db.produtos.createIndex( 
   { descricao: "text" },
-  { default_language: "pt" },
-);
+  { default_language: "portuguese" },
+ );
 
-db.produtos.count(
-  {
-    $text: { $search: "frango hamburger" },
-  },
-);
+ db.produtos.find({ 
+   $text: { 
+     $search: "frango hamburguer",
+   },
+ }).count();
+ 
