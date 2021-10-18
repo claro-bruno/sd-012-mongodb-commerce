051234e81c134c0797759e23544f76db8e92a75b
Eurides <euridesalves@yahoo.com.br>
2021-10-18 19:00:29 -0300

    Adicionado requisito 16

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,10 +1,10 @@
-db.produtos.count(
+db.produtos.updateMany(
     { 
        valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } }, 
      },
     { $push: { tags: "muito sódio" } },
         );
-     
+
 db.produtos.find(
     { },
     { nome: true, tags: true, _id: false },
