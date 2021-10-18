44a086ced086b5a28310407197b194310f42a526
Elisa-Libanio <elisalibanio1@gmail.com>
2021-10-18 19:04:24 -0300

    desafio4,5,6

---
+++ challenges/desafio6.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany({
+    nome: {
+      $in: ["Quarteirão com Queijo", "Big Mac"],
+    },
+  }, {
+    $push: {
+      ingredientes: "bacon",
+    },
+  });
+db.produtos.find({}, {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+  });
