086fe494a04361ca9c0e6c07579b8c7bae41f39c
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-10-18 01:17:11 -0300

    desafio1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,10 +2,10 @@ db.produtos.updateMany(
     {},
     {
         $set: {
-            criadoPor: "Ronald McDonald"
+            criadoPor: "Ronald McDonald",
         }
     }
-)
+);
 db.produtos.find(
     { criadoPor: "Ronald McDonald" },
     { criadoPor: 1, nome: 1, _id: 0 }
