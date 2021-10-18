5a4d16da71f41c3fd6b725caf71fa48cbc6bee49
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-10-18 13:54:36 -0300

    Desafios 15 ao 22

---
+++ challenges/desafio19.js
@@ -0,0 +1,16 @@
+// 19 - Renomeie o campo `descricao` para `descricaoSite` em todos os documentos.
+
+db.produtos.updateMany(
+  {},
+  { $rename: { descricao: "descricaoSite" } },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    descricao: true,
+    descricaoSite: true,
+  },
+);
---
+++ challenges/desafio20.js
@@ -0,0 +1,15 @@
+// 20 - Remova o campo `curtidas` do item `Big Mac`.
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $unset: { curtidas: "" } },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    curtidas: true,
+  },
+);
