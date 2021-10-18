28514e9d359dc6bc7b3db2621e06dcf3630bd794
Guilherme Dias <gmd.delta@gmail.com>
2021-10-18 19:00:45 -0300

    [PT-BR]Resolvido requisito 8 e aleterado updateMany para UpdateOne no requesito 4. [EN] Solved 8th requirement and changed 4th requirement from updateMany to UpdateOne.

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,3 @@
-db.produtos.updateMany({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
   
 db.produtos.find({ ultimaModificacao: { $exists: 1 } }, { _id: 0, nome: 1 });
