2a50d9513c4b8317e0b2ce85ec769bdc9efe3b61
MIkaela Braga <bragamikaela@gmail.com>
2021-10-18 19:09:35 -0300

    desafio 4 completo

---
+++ challenges/desafio4.js
@@ -0,0 +1,5 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: { lastModified: true, ultimaModificacao: { $type: "date" } } },
+  );
+  db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: false, nome: true });
