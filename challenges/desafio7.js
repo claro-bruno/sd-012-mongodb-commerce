7fb43edc691d0b8089342ac5fff3f28249e74613
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-10-19 11:08:56 -0300

    desafio7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,4 +1,8 @@
 db.produtos.updateMany(
     {},
-    { $pull: { ingredientes: "cebola" } }
+    { $pull: { ingredientes: "cebola" } },
 );
+db.produtos.find(
+    {},
+    { _id: 0, nome: 1, ingredientes: 1 }
+)
