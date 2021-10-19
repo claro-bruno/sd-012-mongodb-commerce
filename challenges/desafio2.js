cc248b48c9369d44e578b0ede5b0b7be84c0116d
Vinicius <vini_bodra@hotmail.com>
2021-10-19 11:30:54 -0300

    Requisitos 2 a 12

---
+++ challenges/desafio2.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany(
+  { valorUnitario: { $exists: false } },
+  { $mul: { valorUnitario: NumberDecimal("0.00") } },
+);
+
+db.produtos.find({}, { nome: 1, valorUnitario: 1, _id: 0 });
