ef02775acc283b219c64aa7f05c7caa30815bccd
gabrielfndz <gabriel.omercador@hotmail.com>
2021-10-19 19:16:24 -0300

    Requisito 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,30 @@
+db.produtos.updateMany(
+    {
+      valoresNutricionais: {
+        $elemMatch: {
+          percentual: {
+            $gt: 20,
+            $lt: 40
+          },
+          tipo: {
+            $eq: "sódio",
+          },
+        },
+      },
+    },
+    {
+      $push: {
+        tags: "contém sódio",
+      },
+    },
+  );
+  
+  db.produtos.find(
+    {},
+    {
+      nome: 1,
+      tags: 1,
+      _id: 0,
+    },
+  );
+  
