3a08fde567875d32b11b1c791cb1cdaa6bf9b5cd
Adryan Rosa <adryanrosa374@gmail.com>
2021-10-20 02:20:54 -0300

    Resolve desafio 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,14 @@
+db.produtos.createIndex( 
+  {
+    descricao: "text",
+  },
+  {
+    default_language: "pt",
+  },
+);
+
+db.produtos.count(
+  { 
+    $text: { $search: "\"feito com\"" },
+  },
+);
