6fa05ac2da1dd6cc19950d153bed117f9043ace8
Elisa-Libanio <elisalibanio1@gmail.com>
2021-10-20 10:38:38 -0300

    desafios12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany({}, {
     $push: {
-        valoresNutricionas: {
+        valoresNutricionais: {
         $each: [],
         $sort: { percentual: -1 },
       },
@@ -10,5 +10,5 @@ db.produtos.updateMany({}, {
   db.produtos.find({}, {
     _id: 0,
     nome: 1,
-    valoresNutricionas: 1,
+    valoresNutricionais: 1,
   });
