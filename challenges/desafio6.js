220527358d6b01cb20bee885855083de230a190b
Adryan Rosa <adryanrosa374@gmail.com>
2021-10-19 23:59:58 -0300

    Resolve desafio 7

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -3,7 +3,7 @@ db.produtos.updateMany(
     nome: { $in: ["Big Mac", "Quarteirão com Queijo"] },
   }, 
   {
-     $addToSet: { ingredientes: "bacon" },
+    $addToSet: { ingredientes: "bacon" },
   },
 );
 
