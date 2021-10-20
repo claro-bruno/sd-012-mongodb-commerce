c8117502dacd6ff657e6e0d2e1bc1b17954b3d2c
Adryan Rosa <adryanrosa374@gmail.com>
2021-10-19 23:49:13 -0300

    Resolve desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,18 @@
+db.produtos.updateMany(
+  {
+    nome: "Big Mac",
+  },
+  {
+    $set: { ultimaModificacao: Date() }, 
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: { $exists: true },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
