b4931ea46dd34202d406098fdd1ea1bdac01a02b
Nalu <aluziapantoja@gmail.com>
2021-10-26 18:21:41 -0300

    Req 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,16 @@
+db.produtos.createIndex(
+  {
+    descricao: "text",
+  },
+  {
+    default_language: "pt",
+  },
+);
+
+db.produtos.find(
+  {
+    $text: {
+      $search: "frango hamburguer",
+    },
+  },
+).count();
