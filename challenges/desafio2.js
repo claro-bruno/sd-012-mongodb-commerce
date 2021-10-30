4aa449fdb20a9d24680fff74c264dc17dbd57384
Alice <galice9992@gmail.com>
2021-10-30 05:38:08 -0300

    desafios de 2 a 5 resolvidos

---
+++ challenges/desafio2.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+    {},
+    {
+        $addToSet: { valorUnitario: NumberDecimal("0,00") },
+    },
+);
+
+db.produtos.find(
+    {},
+    {
+        _id: 0, nome: 1, valorUnitario: 1,
+    },
+);
