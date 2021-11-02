c3b87b46ee9cd27cc2d46f6f7a88f0417d8b225d
Ana-Jacomassi <anajacomassip@gmail.com>
2021-11-02 20:59:58 -0300

    desafio 4

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -4,16 +4,16 @@ db.produtos.updateMany(
 );
   
 db.produtos.updateMany(
-  { tags: "bovino" },
-  { $inc: { avaliacao: 5 } },
+    { tags: "bovino" },
+    { $inc: { avaliacao: 5 } },
 );
   
 db.produtos.updateMany(
-  { tags: "ave" },
-  { $inc: { avaliacao: 3 } },
+    { tags: "ave" },
+    { $inc: { avaliacao: 3 } },
 );
   
 db.produtos.find(
-  {},
-  { _id: 0, nome: 1, avaliacao: 1 },
+    {},
+    { _id: 0, nome: 1, avaliacao: 1 },
 );
