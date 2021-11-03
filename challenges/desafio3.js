bff498617e165c454c9fa892fed2ca1b582f4c95
Stefano <stefanooliveira1@gmail.com>
2021-11-03 17:13:22 -0300

    Changes update to updateMany

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -3,12 +3,12 @@ db.produtos.updateMany(
   { $set: { avaliacao: NumberInt("0") } },
 );
 
-db.produtos.update(
+db.produtos.updateMany(
   { tags: "bovino" },
   { $inc: { avaliacao: 5 } },
 );
 
-db.produtos.update(
+db.produtos.updateMany(
   { tags: "ave" },
   { $inc: { avaliacao: 3 } },
 );
