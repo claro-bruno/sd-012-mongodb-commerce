fa2592fac0099042865d220dd83a124d218c9ce3
ladcs <hpladcs@gmail.com>
2021-11-04 13:39:50 -0300

    Request 13 done

---
+++ challenges/desafio13.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  { valoresNutricionais: { $elemMatch: {
+  tipo: "sódio",
+  percentual: {
+    $gte: 40,
+  } } } }, { $push: { tags: {
+    $each: ["muito sódio"],
+  } } },
+);
+
+db.produtos.find(
+  { }, { tags: 1, nome: 1, _id: 0 },
+);
